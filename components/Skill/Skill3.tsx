787bc371dbf0ba0673bad56d3a4554768e91ad79
Roshan Sharma <imrosun@gmail.com>
2022-10-24 14:35:08 +0530

    edited tailwindcss name

--- components/Skill/Skill3.tsx
+++ components/Skill/Skill3.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { motion } from "framer-motion";   
 import Image from "next/image";
-import heroImg from "../images/icon/Tailwindcss.png";  
+import heroImg from "../images/icon/tailwindcss.png";  
 
 type Props = {
     directionLeft?: boolean;
